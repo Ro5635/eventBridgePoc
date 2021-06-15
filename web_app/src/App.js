c7fb40679eaaf8035afc4f2a63c7bb5ea573e927
robert.curran@clicktravel.com <robert.curran@clicktravel.com>
2021-06-15 17:10:38 +0100

    [NO-JIRA] Slight Clean Up To React Page

--- web_app/src/App.js
+++ web_app/src/App.js
@@ -10,25 +10,8 @@ import {
 import eventBridgeEventFlowImage from '../src/images/eventBridgeEventFlow.png';
 
 import Snackbar from '@material-ui/core/Snackbar';
-import MuiAlert from '@material-ui/lab/Alert';
-import { makeStyles } from '@material-ui/core/styles';
-
-function Alert(props) {
-    return <MuiAlert elevation={6} variant="filled" {...props} />;
-}
-
-const useStyles = makeStyles((theme) => ({
-    root: {
-        width: '100%',
-        '& > * + *': {
-            marginTop: theme.spacing(2),
-        },
-    },
-}));
-
 
 function App() {
-    const classes = useStyles();
     const [openToast, setToastOpen] = React.useState(false);
     const wrapCallWithToast = async (call) => {
         // This is not how things should be done in React
